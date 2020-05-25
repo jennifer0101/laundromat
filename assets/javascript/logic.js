858ae8212a401949a7053fe25856648c8e6e904e
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2020-05-25 17:37:48 -0500

    add basic content of pages

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,5 @@
 $(document).ready(function(){
-  $(document).ready(function(){
     $('.parallax').parallax();
-    $('.sidenav').sidenav();
-    $(".dropdown-button").dropdown();
-  });
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
   });
