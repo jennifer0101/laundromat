a9508a9e79b2efb16ed63c16dfd31f7c4b80e21d
Jennifer Zimmerman <jennifer.faye0101@gmail.com>
2020-05-25 16:28:08 -0500

    add parallax

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  $(document).ready(function(){
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $(".dropdown-button").dropdown();
+  });
     $('.sidenav').sidenav();
     $(".dropdown-trigger").dropdown();
   });
