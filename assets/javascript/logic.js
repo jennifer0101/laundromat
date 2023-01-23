3a1a1bfc84d3c8c5c19983541c56610c30d3a9aa
Jennifer Faye <jennifer@fayecreative.com>
2023-01-23 12:23:22 -0600

    changed hero section on each page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
     $('.parallax').parallax();
-    $('.sidenav').sidenav();
+    $('.sidenav').sidenav({edge: 'right' });
+
     $(".dropdown-trigger").dropdown();
   });
 
- 
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); 
+}
